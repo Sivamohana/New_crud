6cf66ac4b8356a17084cb22929c3480bc568150d
Sivamohana <mohanavs291199@gmail.com>
2023-03-24 10:19:40 +0530

    third commit

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+// const URL = 'http://localhost:8000';
+const URL ='https://new-crud.onrender.com'
 
 export const addUser = async (data) =>{
     try
